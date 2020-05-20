86682a86f14ae134600e0f44896e8bde5d90f56c
midasfuld <midas.fuld@knab.nl>
2020-05-20 11:41:16 +0200

    updated readme and added packages

--- README.md
+++ README.md
@@ -17,8 +17,23 @@ In essence a packerbuild to create a standard for Atlas members to work with.
 
 * **A copy of the [Windows 10 x64 Enterprise Trial](https://www.microsoft.com/en-us/evalcenter/evaluate-windows-10-enterprise)**
 * **Packer / Vagrant**  
-* **[Virtualbox](https://www.virtualbox.org/)**
+* **Hyper-v**
 * **An RDP client** 
 
+## Installed features
+* **Hyper-V**
+* **Docker Desktop**
+* **Docker Compose**
+* **VSCode**
+* **Terraform**
+* **AWSCLI**
+* **AWS-Vault**
+* **Cake build**
+* **Chrome**
+* **Firefox**
 
+## How to build
+```
+packer build -force -only=hyperv-iso -var 'iso_path=C:\dump\18363.418.191007-0143.19h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso' -var 'iso_md5=9ef81b6a101afd57b2dbfa44d5c8f7bc94ff45b51b82c5a1f9267ce2e63e9f53' packer.json
+```
 
--- pos_install.ps1
+++ pos_install.ps1
@@ -29,28 +29,32 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -nores
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -norestart
 
 Write-host ">>> Installing VScode"
-choco install -y vscode
+choco install -y vscode --version=1.45.1
 
 Write-host ">>> Installing GIT"
-choco install -y git
+choco install -y git --version=2.26.2
 
 Write-host ">>> Installing AWS-Vault"
-choco install -y aws-vault
+choco install -y aws-vault --version=5.4.3
 
 Write-Host ">>> Installing Terraform"
-choco install -y terraform
+choco install -y terraform --version=0.12.24
 
 Write-host ">>> Installing Docker"
-choco install -y docker-desktop
+choco install -y docker-desktop --version=2.3.0.2
 Write-host ">>> Installing Docker compose"
-choco install -y docker-compose
+choco install -y docker-compose --version=1.25.4
 
 Write-Host ">>> Installing AWS-CLI"
-choco install -y awscli
+choco install -y awscli --version=2.0.14
 
 Write-Host ">>> Installing Extra's like Chrome etc"
 choco install -y Chrome
 choco install -y firefox
+
+Write-Host ">>> Installing Cake"
+choco install -y cake.portable --version=0.37.0
+
 
 Write-Host ">>> Setting up SSH for Vagrant"
 New-Item -ItemType directory -Path ~/.ssh
